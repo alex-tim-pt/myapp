a8f464f3810c64abc19a6583b4fde84ac4e00af9
alex-tim-pt <alex.tim.pt@gmail.com>
2021-09-08 20:09:37 +0300

    'Second'

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ def hello_world():
     """The default controller function"""
     return "<p>Hello, World!</p>"
 
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
+
