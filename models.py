fb7e54c481f5939a0302c6675784072fa8536225
alex-tim-pt <alex.tim.pt@gmail.com>
2021-09-14 23:27:15 +0300

    'S5'

--- models.py
+++ models.py
@@ -2,10 +2,10 @@ import peewee as pw
 
 db = pw.PostgresqlDatabase
 (
-    database = 'dfgdtl6jp8m736'
-    user = 'swutriiwpeabfn'
-    password = 'a308d1808ddb61a6deedb1dbd60432763a9365974f12710c75f3ade0094bea98'
-    host = 'ec2-3-248-103-75.eu-west-1.compute.amazonaws.com'
+    database = 'dfgdtl6jp8m736',
+    user = 'swutriiwpeabfn',
+    password = 'a308d1808ddb61a6deedb1dbd60432763a9365974f12710c75f3ade0094bea98',
+    host = 'ec2-3-248-103-75.eu-west-1.compute.amazonaws.com',
     port = 5432
 )
 
