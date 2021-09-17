1673c301302de8b585a00c2c97879e0c75d388f9
alex-tim-pt <alex.tim.pt@gmail.com>
2021-09-17 19:38:59 +0300

    'S6'

--- models.py
+++ models.py
@@ -1,13 +1,14 @@
 import peewee as pw
 
-db = pw.PostgresqlDatabase
-(
-    database = 'dfgdtl6jp8m736',
-    user = 'swutriiwpeabfn',
-    password = 'a308d1808ddb61a6deedb1dbd60432763a9365974f12710c75f3ade0094bea98',
-    host = 'ec2-3-248-103-75.eu-west-1.compute.amazonaws.com',
-    port = 5432
+
+db = pw.PostgresqlDatabase(
+    database='dfgdtl6jp8m736',
+    user='swutriiwpeabfn',
+    password='a308d1808ddb61a6deedb1dbd60432763a9365974f12710c75f3ade0094bea98',
+    host='ec2-3-248-103-75.eu-west-1.compute.amazonaws.com',
+    port=5432
 )
+
 
 class baseModel(pw.Model):
    class Meta:
